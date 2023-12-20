77a2c0f124b72ce8c97e1354e336fbb4001503aa
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-12-21 00:12:13 +0700

    added meta tags & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,16 +15,23 @@ export default function RootLayout({
 	return (
       <html lang="en">
          <head>
+            {/* META TAGS */}
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta property="og:title" content="Rupiahku" />
+            <meta property="og:description" content="Dompet Digital Kebanggangan Indonesia" />
+            <meta property="og:image" content="https://i.ibb.co/B4pF9r1/rupiahku-logo.png" />
+            <meta property="og:url" content="https://rupiahku.pro/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Rupiahku" />
+            
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
                href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
                rel="stylesheet"
             ></link>
-            <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1"
-            />
             <script
                src="https://challenges.cloudflare.com/turnstile/v0/api.js"
                async
