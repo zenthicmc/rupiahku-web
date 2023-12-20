1604b1e7aeb883877c7a79c977e41b5448e4abd3
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-12-20 23:39:16 +0700

    fix logout build issues

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,10 +2,14 @@
 
 import { useCookies } from "react-cookie"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Logout() {
-	const router = useRouter()
-	const [cookies, setCookie, removeCookie] = useCookies(["token"])
-	removeCookie("token")
-	return router.push("/login")
+	const router = useRouter();
+   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+	
+	useEffect(() => {
+		removeCookie("token");
+		return router.push("/login");
+	}, [])
 }
