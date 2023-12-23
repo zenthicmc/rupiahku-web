d43aa2ad508ee26fcb64f63708008b7cf9bea38d
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-12-23 22:54:00 +0700

    added logout api

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -3,13 +3,19 @@
 import { useCookies } from "react-cookie"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
+import { ApiPost } from "@/utils/api"
 
 export default function Logout() {
 	const router = useRouter();
    const [cookies, setCookie, removeCookie] = useCookies(["token"]);
 	
 	useEffect(() => {
-		removeCookie("token");
-		return router.push("/login");
+		async function logout() {
+			ApiPost("/api/user/logout", cookies.token, {});
+			removeCookie("token")
+			router.push("/login")
+		}
+		
+		logout()
 	}, [])
 }
